21b64b76260cb664bcb67d99742484420fe19d03
martyn <martyn@threephasephoto.co.uk>
2020-09-07 12:10:26 +0100

    don't use mysql if host not available

--- miner_manager_2_backend/mysqlHandler.js
+++ miner_manager_2_backend/mysqlHandler.js
@@ -2,24 +2,33 @@ const mysql = require('mysql2');
 const moment = require('moment');
 
 module.exports.init = async function init(){
-    let pool = mysql.createPool({
-        host:process.env.mysqlHost,
-        user:process.env.mysqlUser,
-        password:process.env.mysqlPass,
-        database:process.env.mysqlTable
-    });
-    global.mysqlConnection = pool.promise();
+    if(process.env.mysqlHost!==""){
+        let pool = mysql.createPool({
+            host:process.env.mysqlHost,
+            user:process.env.mysqlUser,
+            password:process.env.mysqlPass,
+            database:process.env.mysqlTable
+        });
+        global.mysqlConnection = pool.promise();
+    }
 }
 
 module.exports.hiveToken = async function hiveToken(setTokenTo,setTokenDies=moment().add(14,"days")){
     if(typeof setTokenTo==="undefined"){
-        let [token] = await global.mysqlConnection.query("SELECT hiveToken,hiveTokenDies FROM config;");
-        // console.log(token);
-        return token[0];
+        if(process.env.mysqlHost!==""){
+            let [token] = await global.mysqlConnection.query("SELECT hiveToken,hiveTokenDies FROM config;");
+            // console.log(token);
+            return token[0];
+        }
+        else{
+            return "";
+        }
     }
     else{
-        console.log("setting db hive token to %s",setTokenTo);
-        await global.mysqlConnection.query("UPDATE config SET ?;",[{hiveToken:setTokenTo,hiveTokenDies:moment(setTokenDies).toDate()}]);
+        if(process.env.mysqlHost!==""){
+            console.log("setting db hive token to %s",setTokenTo);
+            await global.mysqlConnection.query("UPDATE config SET ?;",[{hiveToken:setTokenTo,hiveTokenDies:moment(setTokenDies).toDate()}]);
+        }
     }
 }
 
